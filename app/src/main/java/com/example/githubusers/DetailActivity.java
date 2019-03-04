9da1f19f9df62da3a76ca4f51ea929340dc4611b
Leonard O <leo@Leonards-MacBook-Pro.local>
2019-03-04 15:20:56 +0300

    feat(intents): Launch detail activity from main activity - Opens list item details in another view
    [Delivers #155820624]

--- app/src/main/java/com/example/githubusers/DetailActivity.java
+++ app/src/main/java/com/example/githubusers/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.githubusers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Get the caller intent
+        Intent intent = getIntent();
+        String extra_username = intent.getStringExtra("EXTRA_USERNAME");
+
+        //Set title bar text
+        getSupportActionBar().setTitle(extra_username);
+
+        TextView tvUserName = findViewById(R.id.tvUserName);
+        TextView tvFirstName = findViewById(R.id.tvFirstName);
+        TextView tvLastName = findViewById(R.id.tvLastName);
+        TextView tvRepos = findViewById(R.id.tvRepos);
+        TextView tvBio = findViewById(R.id.tvBio);
+
+        tvUserName.setText(extra_username);
+        tvFirstName.setText("");
+        tvLastName.setText("");
+        tvRepos.setText("");
+        tvBio.setText("");
+
     }
 }
--- app/src/main/java/com/example/githubusers/MainActivity.java
+++ app/src/main/java/com/example/githubusers/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.githubusers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] githubUsers ={
+            "@leo",
+            "@johngorithm",
+            "@codeplus254",
+            "@jb43daysGalore",
+            "@nateLEHI",
+            "@pendo_elizabeth",
+            "@trina",
+            "@tiffany",
+            "@sanchez",
+            "@eliza",
+            "@monalisa",
+            "@dilan"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView) findViewById(R.id.lvUsers);
+
+        /**
+         * Assign adapter to ListView
+         * */
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, githubUsers);
+        listView.setAdapter(adapter);
+
+        /**
+         * Assign listener to ListView
+         * */
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                int itemPosition = position;
+                String userName = (String) listView.getItemAtPosition(position);
+
+                Toast.makeText(getApplicationContext(), "You clicked "+userName+" at position "+itemPosition+"", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                intent.putExtra("EXTRA_USERNAME", userName);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    public void launchUserDetails(View view){
+        //
     }
 }
