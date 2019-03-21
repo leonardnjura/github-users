7fe7c96244027af698146451eeb5076c36d98012
Leo <leonard.njura@gmail.com>
2019-03-21 13:56:40 +0300

    (fix): Fix build

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ What?
 
 How?
 ----
-> To test this app on your local machine or device, follow these guidelines:
+> To test this app here on your local machine or device, follow these guidelines:
 
 * Download and install Android Studio
 * Clone this repo and open in above IDE
--- app/src/main/java/com/example/githubusers/view/DetailActivity.java
+++ app/src/main/java/com/example/githubusers/view/DetailActivity.java
@@ -26,6 +26,7 @@ public class DetailActivity extends AppCompatActivity implements UserDetailParen
     public static String USER_INFO_KEY = "userData";
     public UserProfile ghUserInfo;
     public String userName;
+    public String atSignUsername;
     public ProgressDialog progressDialog;
 
     private final GithubService service = new GithubService();
@@ -42,10 +43,20 @@ public class DetailActivity extends AppCompatActivity implements UserDetailParen
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent incomingIntent = getIntent();
 
+
+        //Get the caller intent
+        Intent intent = getIntent();
+        userName = intent.getStringExtra("EXTRA_USERNAME");
+
+
+        // Set title bar text
+        atSignUsername = this.getResources().getString(R.string.at_sign_username, userName);
+        getSupportActionBar().setTitle(atSignUsername);
+
+
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Loading...");
         progressDialog.show();
-
 
 
 
@@ -69,15 +80,10 @@ public class DetailActivity extends AppCompatActivity implements UserDetailParen
     private void fetchData(Intent newIntent) {
         // HANDLING INCOMING INTENT
         if (newIntent.hasExtra("EXTRA_USERNAME")) {
-            String userName = newIntent.getStringExtra("EXTRA_USERNAME");
 
             UserProfilePresenter profilePresenter = new UserProfilePresenter(this, service);
             profilePresenter.getUserProfile(userName);
             Log.i(TAG, "USERNAME:: " +userName);
-
-            // Set title bar text
-            String atSignUsername = this.getResources().getString(R.string.at_sign_username, userName);
-            getSupportActionBar().setTitle(atSignUsername);
 
         } else {
             Toast toast = Toast.makeText(getApplicationContext(), "Invalid username, Please try again", Toast.LENGTH_LONG);
@@ -115,8 +121,9 @@ public class DetailActivity extends AppCompatActivity implements UserDetailParen
                 .load(userProfile.getAvatarUrl())
                 .apply(RequestOptions.bitmapTransform(new RoundedCorners(1)))
                 .into(userImageElement);
+
         // API Required fields
-        userNameElement.setText(userProfile.getUserName());
+        userNameElement.setText(atSignUsername);
         publicReposElement.setText(userProfile.getPublicRepos());
         followingElement.setText(userProfile.getFollowing());
         followersElement.setText(userProfile.getFollowers());
@@ -152,19 +159,30 @@ public class DetailActivity extends AppCompatActivity implements UserDetailParen
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.action_share) {
-            Toast.makeText(this, "Share option selected..", Toast.LENGTH_SHORT).show();
-            if (this.userName != null) {
-                String text = "Checkout this awesome developer @" + this.userName + "\nhttps://github.com/" + this.userName;
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_TEXT, text);
-                startActivity(Intent.createChooser(intent, "Share via"));
-            } else {
-                Toast.makeText(this, "Shareable user data not found", Toast.LENGTH_SHORT).show();
-            }
+        switch (item.getItemId()){
+            case R.id.action_share:
+                Toast.makeText(this, "Share option selected..", Toast.LENGTH_SHORT).show();
+                if (this.userName != null) {
+                    String text = "Checkout this awesome developer @" + this.userName + "\nhttps://github.com/" + this.userName;
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_TEXT, text);
+                    startActivity(Intent.createChooser(intent, "Share via"));
+                } else {
+                    Toast.makeText(this, "Shareable user data not found", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case R.id.home:
+                Toast.makeText(this, "Home option selected..", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refresh option selected..", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return true;
+
+
     }
 
     @Override
